0ee1e8c65ec020a2c65e0d50c03784d79df4d7c4
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2019-04-01 20:41:03 -0300

    Adding api google maps request
    Co-authored-by: Micaella Gouveia <micaella2212@gmail.com>
    Co-authored-by: Luis Taira <luishptaira@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 /* eslint-disable import/no-unresolved */
 const express = require('express');
 const bodyParser = require('body-parser');
+const https = require('https');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.json({ Hello: 'World' });
+let data = '';
+https.get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOURKEY', (resp) => {
+  
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+  
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+  
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
 });
 
+
+app.get('/', (req, res) => {
+  res.json( JSON.parse(data) );
+});
 app.listen(3001);
