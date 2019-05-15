4a6469ac7bca9065612b6fcfa698bf9a6d2b907b
Sam Buters <sambuters@gmail.com>
2019-05-15 11:42:57 -0300

    Adding similar results treatment

--- src/requests/requestCoords.js
+++ src/requests/requestCoords.js
@@ -4,6 +4,101 @@ const haversine = require('../utils/haversine');
 
 const key = process.env.API_KEY;
 
+function treatResults(results, selector, userInput) {
+  let highestIndex;
+  let highestScore = -100;
+  userInput = userInput.toLowerCase();
+
+  if(selector.length == 1) {
+    return selector[0];
+  }
+
+  selector.forEach((index) => {
+    if(results[index].components.country) {
+      if(new RegExp((results[index].components.country).toLowerCase()).test(userInput)) {
+        results[index].score += 1;
+      }
+
+      else {
+        if(results[index].components.country_code) {
+          if(new RegExp(`\\b${(results[index].components.country_code).toLowerCase()}\\b`).test(userInput)) {
+            results[index].score += 1;
+          }
+
+          else {
+            results[index].score -= 1;
+          }
+        }
+
+        else {
+          results[index].score -= 1
+        }
+      }
+    }
+
+      if(results[index].components.state) {
+          if(new RegExp((results[index].components.state).toLowerCase()).test(userInput)) {
+            results[index].score += 1;
+          }
+
+          else {
+            if(results[index].components.state_code) {
+              if(new RegExp(`\\b${(results[index].components.state_code).toLowerCase()}\\b`).test(userInput)) {
+                results[index].score += 1;
+              }
+
+              else {
+                results[index].score -= 1;
+              }
+            }
+
+            else {
+              results[index].score -= 1
+            }
+          }
+        }
+
+      if(results[index].components.city) {
+          if(new RegExp((results[index].components.city).toLowerCase()).test(userInput)) {
+            results[index].score += 1;
+          }
+
+          else {
+            if(results[index].components.city_code) {
+              if(new RegExp(`\\b${(results[index].components.city_code).toLowerCase()}\\b`).test(userInput)) {
+                results[index].score += 1;
+              }
+
+              else {
+                results[index].score -= 1;
+              }
+            }
+
+            else {
+              results[index].score -= 1
+            }
+          }
+        }
+
+      if(results[index].components.road) {
+        if(new RegExp((results[index].components.road).toLowerCase()).test(userInput)) {
+          results[index].score += 1;
+        }
+
+        else {
+          results[index].score -= 1;
+        }
+      }
+
+      if(results[index].score > highestScore) {
+        highestScore = results[index].score;
+        highestIndex = index;
+      }
+  });
+
+  return highestIndex;
+}
+
 function toRadians(num) {
   return num * (Math.PI/180);
 }
@@ -18,7 +113,7 @@ function bodyToLocal(body, local) {
   }
 }
 
-function bodyToResultsArray(body) {
+function bodyToResultsArray(body, userInput) {
   try {
     const { results } = body;
     const { total_results : totalResults } = body;
@@ -27,6 +122,7 @@ function bodyToResultsArray(body) {
 
     results.forEach((value, index) => {
       results[index].isChecked = 0;
+      results[index].score = 0;
     });
 
     results.forEach((value, index) => {
@@ -43,12 +139,24 @@ function bodyToResultsArray(body) {
 
             if(haversine.distance(lat, lng) <= radius) {
               selector.push(index2);
+              console.log('close');
             }
           }
         });
 
-        //resultsArray.push(results[treatResults(results, selector)]);
+        resultsArray.push(results[treatResults(results, selector, userInput)]);
       }
+    });
+
+   resultsArray.forEach((value, index) => {
+      delete resultsArray[index].annotations;
+      delete resultsArray[index].bounds;
+      delete resultsArray[index].components;
+      delete resultsArray[index].confidence;
+      delete resultsArray[index].isChecked;
+      delete resultsArray[index].score;
+      resultsArray[index].lat = resultsArray[index].geometry.lat;
+      resultsArray[index].lng = resultsArray[index].geometry.lng;
     });
 
     return resultsArray;
@@ -63,13 +171,13 @@ module.exports = {
     let data = '';
     let results;
     return new Promise((resolve) => {
-      https.get(`https://api.opencagedata.com/geocode/v1/json?q=${name}&key=${key}`, (resp) => {
+      https.get(`https://api.opencagedata.com/geocode/v1/json?q=${name}&key=${key}&language=pt-BR`, (resp) => {
         resp.on('data', (chunk) => {
           data += chunk;
         });
 
         resp.on('end', () => {
-          results = bodyToResultsArray(JSON.parse(data));
+          results = bodyToResultsArray(JSON.parse(data), name);
           resolve(results);
         });
       });
