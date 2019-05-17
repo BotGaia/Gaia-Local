8f579f888de2ea2edaae2e56190903ca434035e2
Micaella <micaella2212@gmail.com>
2019-05-17 19:34:47 -0300

    Changing name os tests and routes variables

--- src/test/requestCoordsTest.js
+++ src/test/requestCoordsTest.js
@@ -7,18 +7,19 @@ const Coords = require('../requests/requestCoords.js');
 
 const should = chai.should();
 
-describe('List', () => {
-  it('get Latitude', (done) => {
-    Coords.getLocales('brasilia').then((value) => {
-      const { lat } = value[0];
+describe('Coords List', () => {
+  it('get first value from latitude list', (done) => {
+    Coords.getLocales('brasilia').then((listLatitude) => {
+      const { lat } = listLatitude[0];
       should.equal(lat, -10.3333333);
       done();
     });
+
   }).timeout(5000);
 
-  it('get Longitude', (done) => {
-    Coords.getLocales('brasilia').then((value) => {
-      const { lng } = value[0];
+  it('get first value from latitude list', (done) => {
+    Coords.getLocales('brasilia').then((listLongitude) => {
+      const { lng } = listLongitude[0];
       should.equal(lng, -53.2);
       done();
     });
@@ -27,16 +28,16 @@ describe('List', () => {
 
 describe('Coords', () => {
   it('get Latitude', (done) => {
-    Coords.getCoords('parana').then((value) => {
-      lat = value.getLatitude();
+    Coords.getCoords('parana').then((local) => {
+      lat = local.getLatitude();
       should.equal(lat, '-24.4842187');
       done();
     });
   }).timeout(5000);
 
   it('get Longitude', (done) => {
-    Coords.getCoords('parana').then((value) => {
-      lon = value.getLongitude();
+    Coords.getCoords('parana').then((local) => {
+      lon = local.getLongitude();
       should.equal(lon, '-51.8148872');
       done();
     });
